b01f675fb78c8cfa2ab3588bfb41488270e30a5c
Thiago Martins <thiago.fmartins@outlook.com>
2024-05-29 11:53:11 -0300

    Change email service to use lib "messages"

--- src/business/message_sender/email_sender.py
+++ src/business/message_sender/email_sender.py
@@ -1,8 +1,7 @@
 from utils import BaseService
-import smtplib, base64
-from email.message import EmailMessage
 from src import config as cfg
 from typing import Optional
+from messages import Email
 
 
 class EmailSender(BaseService):
@@ -22,35 +21,18 @@ class EmailSender(BaseService):
         cc: Optional[list[str]] = None,
         bcc: Optional[list[str]] = None,
         attachments: Optional[list[str, str]] = None,
-        html: bool = False,
     ):
-        msg = EmailMessage()
-
-        if html:
-            msg.add_alternative(body, subtype="html")
-        else:
-            msg.set_content(body)
-
-        msg["Subject"] = subject
-        msg["From"] = self.username
-        msg["To"] = ", ".join(to)
-        if cc:
-            msg["Cc"] = ", ".join(cc)
-        if bcc:
-            msg["Bcc"] = ", ".join(bcc)
-
-        if attachments:
-            for file_name, encoded_content in attachments.items():
-                decoded_content = base64.b64decode(encoded_content)
-                msg.add_attachment(
-                    decoded_content,
-                    maintype="application",
-                    subtype="octet-stream",
-                    filename=file_name,
-                )
-
-        with smtplib.SMTP_SSL(cfg.SMTP.server, cfg.SMTP.port) as smtp_conn:
-            smtp_conn.login(self.username, self.password)
-            smtp_conn.send_message(msg)
-
-        return "Email sent successfully"
+        msg = Email(
+            from_=self.username,
+            to=to,
+            auth=self.password,
+            cc=cc,
+            bcc=bcc,
+            subject=subject,
+            body=body,
+            attachments=attachments,
+            verbose=self.verbose,
+        )
+        msg.send()
+        self._log("Email sent successfully")
+        return True
--- tests/test_email_sender.py
+++ tests/test_email_sender.py
@@ -3,11 +3,7 @@ from src.business.message_sender import EmailSender
 
 
 def test_send_email():
-    with open("./src/business/utils.py", "rb") as file:
-        encoded_content = base64.b64encode(file.read())
-    base64_content_utils = encoded_content.decode("utf-8")
-
-    sender = EmailSender()
+    sender = EmailSender(verbose=True)
     sender.send_email(
         to=["foo.bar@bar.com"],
         subject="Test Email",
@@ -15,8 +11,6 @@ def test_send_email():
         cc=["foo@bar.com"],
         # bcc=["bcc@example.com"],
         attachments={
-            "utils2.py": base64_content_utils,
-            "utils.py": base64_content_utils,
+            "utils.py": "./src/business/utils.py",
         },
-        html=True,
     )
