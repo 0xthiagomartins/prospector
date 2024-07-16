b2213ac7f2e39049fe3bd016f3052092b49c4968
Thiago Martins <thiago.fmartins@outlook.com>
2024-07-16 10:49:00 -0300

    DEL: unused test

--- tests/test_prospector_service.py
+++ tests/test_prospector_service.py
@@ -5,13 +5,11 @@ def test_get_emails_from_domain(domain: str):
     return Prospector(verbose=True).list_emails_from_domain(domain)
 
 
-if __name__ == "__main__":
-    domain = input("Input your domain: ")
-    emails = Prospector(verbose=True).list_emails_from_domain(domain)
-
-
 def emails_to_string(email_list):
     return ", ".join(email_list)
 
 
-print(emails_to_string(emails))
+if __name__ == "__main__":
+    domain = input("Input your domain: ")
+    emails = Prospector(verbose=True).list_emails_from_domain(domain)
+    print(emails_to_string(emails))
