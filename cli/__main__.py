5237056a8df49c4cd93fd8a3024ef4a039718297
Thiago Martins <thiago.fmartins@outlook.com>
2024-09-23 12:40:31 -0300

    refactor cli with app instance

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,20 +1,20 @@
-import typer
-import os
-import sys
+import os, sys
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from typer import Typer, Option
+from rich import print
+from src.service import Prospector
 
-def get_emails(domain: str):
-    from src.service import Prospector
+app = Typer()
 
-    def emails_to_string(email_list):
-        return ", ".join(email_list)
 
+@app.command()
+def get_emails(domain: str = Option(..., help="The user ID to list conversations for")):
     emails = Prospector(verbose=True).list_emails_from_domain(domain)
-    print(emails_to_string(emails))
+    print(", ".join(emails))
 
 
 if __name__ == "__main__":
-    typer.run(get_emails)
+    app()
